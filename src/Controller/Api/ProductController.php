fb8ec7c8752d8809d8ed493e263fab483c777730
Denis Brumann <denis.brumann@sensiolabs.de>
2018-05-19 08:28:15 +0200

    Create endpoint for listing products.

---
+++ src/Controller/Api/ProductController.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @Route("/api", name="api_")
+ */
+final class ProductController
+{
+    /**
+     * @Route("/products", name="products_list", methods={"GET"})
+     */
+    public function listProducts()
+    {
+        return new JsonResponse([]);
+    }
+}
---
+++ tests/Controller/Api/ProductControllerTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types = 1);
+
+namespace App\Tests\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProductControllerTest extends WebTestCase
+{
+    public function test_products_list_is_accessible()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/products');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+    }
+}
